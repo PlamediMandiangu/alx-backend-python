ddf31c5e0e77b2c2de0f395c4b27e4b135cacfe2
unknown <plamedimandiangu937@gmail.com>
2025-08-15 00:24:44 +0200

    Add RequestLoggingMiddleware and RestrictAccessByTimeMiddleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,8 +1,9 @@
-# chats/middleware.py
+# Django-Middleware-0x03/chats/middleware.py
 
 from datetime import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 
+# Middleware 1: Logs each user request
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -14,6 +15,20 @@ class RequestLoggingMiddleware:
         # Log to file
         with open("requests.log", "a") as f:
             f.write(log_message)
+        
+        response = self.get_response(request)
+        return response
+
+# Middleware 2: Restrict access to chat between 6 PM and 9 PM
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        # Deny access if time is before 6 PM (18) or after 9 PM (21)
+        if current_hour < 18 or current_hour > 21:
+            return HttpResponseForbidden("Chat access is restricted between 9 PM and 6 PM.")
         
         response = self.get_response(request)
         return response
