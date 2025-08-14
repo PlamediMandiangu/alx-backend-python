906b6b8c99ab30f5f66091e41ea2d7510ee1e172
unknown <plamedimandiangu937@gmail.com>
2025-08-14 23:25:50 +0200

    Save the chats file

--- Django-Middleware-0x03/messaging_app/asgi.py
+++ Django-Middleware-0x03/messaging_app/asgi.py
@@ -1,6 +1,17 @@
+"""
+ASGI config for messaging_app project.
+
+It exposes the ASGI callable as a module-level variable named `application`.
+
+For more information, see:
+https://docs.djangoproject.com/en/4.3/howto/deployment/asgi/
+"""
+
 import os
 from django.core.asgi import get_asgi_application
 
+# Set default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
 
+# ASGI application
 application = get_asgi_application()
--- Django-Middleware-0x03/messaging_app/wsgi.py
+++ Django-Middleware-0x03/messaging_app/wsgi.py
@@ -1,6 +1,17 @@
+"""
+WSGI config for messaging_app project.
+
+It exposes the WSGI callable as a module-level variable named `application`.
+
+For more information, see:
+https://docs.djangoproject.com/en/4.3/howto/deployment/wsgi/
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Set default settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
 
+# WSGI application
 application = get_wsgi_application()
