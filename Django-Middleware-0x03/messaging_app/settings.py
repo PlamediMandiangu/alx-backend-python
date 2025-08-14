00d1930bafb7be74ab7d82f18ba27ee98ce96d2e
unknown <plamedimandiangu937@gmail.com>
2025-08-15 00:11:14 +0200

    Restrict Chat Access by time

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -32,17 +32,24 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
+    # Your custom middleware
+    "chats.middleware.RequestLoggingMiddleware",
 ]
+
+
 MIDDLEWARE += [
     'chats.middleware.RequestLoggingMiddleware',
 ]
+
+
 
 ROOT_URLCONF = 'messaging_app.urls'
 
